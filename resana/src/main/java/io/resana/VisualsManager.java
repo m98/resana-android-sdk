f41cd3d6e3525a859af9206c12f446d2f8a0f5af
Ehsan <ehsansouri23@gmail.com>
2018-12-19 13:44:40 +0330

    add java doc. check if ad type is not native

--- resana/src/main/java/io/resana/VisualsManager.java
+++ resana/src/main/java/io/resana/VisualsManager.java
@@ -13,7 +13,15 @@ import static io.resana.ResanaPreferences.saveString;
 class VisualsManager {
     private static final String TAG = "VisualsManager";
 
+    /**
+     * This method will save visual indexes of an ad.
+     * after we can get an index of a visual randomly
+     * @param context
+     * @param ad
+     */
     static void saveVisualsIndex(Context context, Ad ad) {
+        if (ad.getType() != AdDto.AD_TYPE_NATIVE)
+            return;
         List<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < ((NativeDto) ad.data).visuals.size(); i++) {
             indexes.add(i);
@@ -26,6 +34,12 @@ class VisualsManager {
         saveString(context, ad.getId() + PREF_VISUAL_INDEX, indexString);
     }
 
+    /**
+     * This method will return list of visual indexes of an ad which is shuffled.
+     * @param context
+     * @param ad
+     * @return
+     */
     static List<Integer> getDownloadingVisualsIndex(Context context, Ad ad) {
         List<Integer> indexes = new ArrayList<>();
         String[] indexesString = getString(context, ad.getId() + PREF_VISUAL_INDEX, "").split(",");
@@ -35,6 +49,12 @@ class VisualsManager {
         return indexes;
     }
 
+    /**
+     * This method will randomly return an index of a visual.
+     * @param context
+     * @param ad
+     * @return
+     */
     static int getVisualIndex(Context context, NativeAd ad) {
         String indexString = getString(context, ad.getOrder() + PREF_VISUAL_INDEX, "");
         String indexString2 = indexString.split(",")[0];
