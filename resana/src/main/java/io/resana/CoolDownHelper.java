7bb7a26ca7c0b58dcfe8a0439c36377af66c3da1
Ehsan <ehsansouri23@gmail.com>
2018-12-20 12:58:19 +0330

    remove subtitle cooldown settings

--- resana/src/main/java/io/resana/CoolDownHelper.java
+++ resana/src/main/java/io/resana/CoolDownHelper.java
@@ -14,68 +14,15 @@ import static io.resana.ResanaPreferences.PREF_CTRL_SPLASH_COOL_DOWN_FIRST_CHANC
 import static io.resana.ResanaPreferences.PREF_CTRL_SPLASH_COOL_DOWN_FIRST_CHANCE_TS;
 import static io.resana.ResanaPreferences.PREF_CTRL_SPLASH_COOL_DOWN_TS;
 import static io.resana.ResanaPreferences.PREF_CTRL_SPLASH_COOL_DOWN_TTL;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_CHANCE;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE_INTERVAL;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE_TS;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_MAX;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_MIN;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_TS;
-import static io.resana.ResanaPreferences.PREF_CTRL_SUBTITLE_COOL_DOWN_TTL;
 import static io.resana.ResanaPreferences.getFloat;
 import static io.resana.ResanaPreferences.getInt;
 import static io.resana.ResanaPreferences.getLong;
-import static io.resana.ResanaPreferences.getPrefs;
 import static io.resana.ResanaPreferences.remove;
 import static io.resana.ResanaPreferences.saveFloat;
 import static io.resana.ResanaPreferences.saveInt;
 import static io.resana.ResanaPreferences.saveLong;
 
 class CoolDownHelper {
-    private static final int DEFAULT_SUBTITLE_COOL_DOWN_MAX = 35;
-    private static final int DEFAULT_SUBTITLE_COOL_DOWN_MIN = 15;
-    private static final int DEFAULT_SUBTITLE_FIRST_WAIT_TIME = 10;
-
-    static int getDefaultSubtitleWaitTime(boolean first) {
-        if (first)
-            return DEFAULT_SUBTITLE_FIRST_WAIT_TIME * 1000;
-        else
-            return (DEFAULT_SUBTITLE_COOL_DOWN_MAX + DEFAULT_SUBTITLE_COOL_DOWN_MIN) / 2 * 1000;
-    }
-
-    static boolean shouldShowSubtitle(Context context) {
-        validateSubtitleCoolDownData(context);
-        if (isFirstSubtitle(context)) {
-            saveLong(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE_TS, System.currentTimeMillis());
-            final float first = getFloat(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE, 1f);
-            return Math.random() < first;
-        }
-        final float chance = getPrefs(context).getFloat(PREF_CTRL_SUBTITLE_COOL_DOWN_CHANCE, 1f);
-        return Math.random() < chance;
-    }
-
-    private static boolean isFirstSubtitle(Context context) {
-        final int interval = getInt(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE_INTERVAL, -1);
-        final float first = getFloat(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE, -1);
-        if (interval < 0 || first < 0)
-            return false;
-        final long lastTime = getLong(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE_TS, -1);
-        return System.currentTimeMillis() > lastTime + interval * 1000;
-    }
-
-    static int getSubtitleFirstWait(Context context) {
-        validateSubtitleCoolDownData(context);
-        final int fWait = getPrefs(context).getInt(PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST, DEFAULT_SUBTITLE_FIRST_WAIT_TIME) * 1000;
-        return (int) (Math.random() * fWait * 2);
-    }
-
-    static int getSubtitleWait(Context context) {
-        validateSubtitleCoolDownData(context);
-        final int min = getInt(context, PREF_CTRL_SUBTITLE_COOL_DOWN_MIN, DEFAULT_SUBTITLE_COOL_DOWN_MIN) * 1000;
-        final int max = getInt(context, PREF_CTRL_SUBTITLE_COOL_DOWN_MAX, DEFAULT_SUBTITLE_COOL_DOWN_MAX) * 1000;
-        return min + (int) (Math.random() * (max - min));
-    }
 
     static boolean shouldShowSplash(Context context) {
         validateSplashCoolDownData(context);
@@ -116,24 +63,6 @@ class CoolDownHelper {
         if (interval < 0 || first < 0)
             return false;
         return System.currentTimeMillis() > lastTime + interval * 1000;
-    }
-
-    static private void validateSubtitleCoolDownData(Context context) {
-        final long ts = getPrefs(context).getLong(PREF_CTRL_SUBTITLE_COOL_DOWN_TS, -1);
-        final long ttl = getPrefs(context).getInt(PREF_CTRL_SUBTITLE_COOL_DOWN_TTL, -1);
-        if (ts < 0 || ttl < 0)
-            return;
-        if (System.currentTimeMillis() < ts + ttl * 1000)
-            return;
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_MIN);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_MAX);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_CHANCE);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_TS);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_TTL);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE_TS);
-        remove(context, PREF_CTRL_SUBTITLE_COOL_DOWN_FIRST_CHANCE_INTERVAL);
     }
 
     static private void validateSplashCoolDownData(Context context) {
