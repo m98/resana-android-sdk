6ecba7ac58a76db246d33ee8807121497f3b0457
Ehsan <ehsansouri23@gmail.com>
2018-12-23 10:35:50 +0330

    rename network manager test

--- resana/src/androidTest/java/io/resana/NetworkManagerTest.java
+++ resana/src/androidTest/java/io/resana/NetworkManagerTest.java
@@ -5,19 +5,19 @@ import android.util.Log;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 
-public class NetworkHelperTest {
-    private static final String TAG = "NetworkHelper";
+import static org.junit.Assert.assertEquals;
+
+public class NetworkManagerTest {
+    private static final String TAG = "NetworkManager";
 
     @Test
     public void openConnectionTest() throws IOException {
         String url = "http://tpsl.ir/TZBXR";
         int responseCode = 0;
         try {
-            responseCode = NetworkHelper.openConnection(url).getResponseCode();
+            responseCode = NetworkManager.openConnection(url).getResponseCode();
         } catch (Exception e) {
             Log.e(TAG, "error", e);
         }
